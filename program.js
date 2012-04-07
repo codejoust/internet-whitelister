b18048d35c9d7ef9b8b2b23670403c067be7c6a4
Iain Nash <iain@iain.in>
2012-04-06 21:09:00 -0400

    Adding in real-time components, audit log

--- program.js
+++ program.js
@@ -2,7 +2,7 @@
 
 var stdin = process.openStdin()
   , url = require('url')
-  , redis = require('redis-node')
+  , redis = require('redis')
   , redis_conn = redis.createClient()
   , writeout = console.log;
 
@@ -34,7 +34,9 @@ function check_user_allowed(ip, ok, fail){
 	if (!ip){ fail(); return; }
 	redis_conn.get('u:ip:' + ip, function(err, data){
 		if (err){ fail(); }
-		else if (data == 'free') { ok(); }
+		else if (data == 'free') { 
+			ok(); 
+		}
 		else { fail(); }
 	})
 }
@@ -44,7 +46,7 @@ stdin.on('data', function(chunk){
   var input_raw = (chunk + '').split(' ');
   var request = {url_raw: input_raw.shift(), ip: input_raw.shift(), ident: input_raw.shift(), method: input_raw.shift()};
   if (request.ip){
-  	request.ip = request.ip.trimRight();
+  	request.ip = request.ip.match(/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/)[0];
   }
   request.url = url.parse(request.url_raw);
   if (request.url['hostname']){
@@ -52,6 +54,7 @@ stdin.on('data', function(chunk){
 	  writeout("True");
 	}, function(){
   	  check_allowed_host(request.url['hostname'], function(){
+       redis_conn.publish('visits', JSON.stringify({ip: request.ip, host: request.url['hostname'], date: new Date().getTime()}));
        writeout("True");
 	  }, function(type){
        writeout((request.method == 'CONNECT') ? '302:' : '' + "http://localhost/pproxy.php?h=" + request.url['hostname'] + "&u=" + request.url_raw + '&m=' + request.method);
--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 var express = require('express')
-  , redis = require('redis-node')
+  , redis = require('redis')
   , redis_conn = redis.createClient()
-  , app = express.createServer();
+  , app = express.createServer()
+  ,	sio = require('socket.io')
+  , io = sio.listen(app);
+
+io.set('log level', 1);
 
 app.use(express.bodyParser());
+app.use(express.logger());
+app.use(function(req, res, next){
+  if (req.headers['Via'] && req.headers['Via'].match(/squid/i)){
+	res.writeHead(405, {'Content-Type': 'text/plain'});
+	res.end('Not Allowed');
+  } else {
+    next();
+  }
+});
+
+io.sockets.on('connection', function(socket){
+	var sub_conn = redis.createClient();
+	sub_conn.on('message', function(channel, message){
+	  socket.emit('visits', message);
+	});
+	sub_conn.subscribe('visits');
+});
 
 app.get('/', function(req, res){
 	var sort = get_sort(req.param('find'))
@@ -22,8 +43,28 @@ app.get('/access', function(req,res){
 	res.send('/access/(ip)/(action=free/deny)');
 });
 
+function get_ip(req){
+  return req.socket && (req.socket.remoteAddress || (req.socket.socket && req.socket.socket.remoteAddress));
+}
+
+
+app.get('/audit', function(req, res){
+	var per_page  = (req.params.page && parseInt(req.params.page)) ? parseInt(req.params.page) : 0;
+	var page_size = 120;
+	redis_conn.lrange('connaudit', per_page*page_size, 120 + per_page*page_size, function(err, data){
+		res.render('audits.jade', {audits: data.map(function(audit_entry){
+			try {
+			  return JSON.parse(audit_entry);
+			} catch (e){ return null; };
+		})});
+	});
+});
+
 app.get('/access/:ip/:action', function(req, res){
 	if (req.params.ip && req.params.action){
+	  redis_conn.lpush('connaudit', JSON.stringify({date: new Date(), act: req.params.action, host: req.params.ip, from: get_ip(req)}), function(err, ok){
+	  	  if (err) console.err(err);
+		});
 	  redis_conn.set('u:ip:' + req.params.ip, req.params.action, function(err, ok){
 		if (err){ res.send('err setting: ' + err) }
 		else { res.send('ok: ip (' + req.params.ip + ') action '+req.params.action); }
@@ -38,8 +79,11 @@ app.get('/accepted', function(req,res){
 });
 
 app.post('/update', function(req, res){
-	if (req.body.domain){
+	if (req.body.domain && req.body.action){
 		var domain = req.body.domain;
+	  redis_conn.lpush('connaudit', JSON.stringify({date: new Date(), act: req.body.action, host: req.body.domain, from: get_ip(req)}), function(err, ok){
+	  	  if (err) console.err(err);
+		});
 		if (req.body.action == 'accept'){
 			redis_conn.zadd('resdomains', 1, req.body.domain, function(err){
 				//res.send({status: err});	
